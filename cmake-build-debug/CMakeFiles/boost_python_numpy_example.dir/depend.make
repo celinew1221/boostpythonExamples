33db422af94314c0cf9e75ca923348effd475e31
Celine Wei <c.wei@surgicalsafety.com>
2019-03-27 11:24:39 -0400

    add CMakeLists.txt

---
+++ cmake-build-debug/CMakeFiles/boost_python_numpy_example.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for boost_python_numpy_example.
+# This may be replaced when dependencies are built.
---
+++ cmake-build-debug/CMakeFiles/boostpythonExamples.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/boostpythonExamples.dir/test.cpp.o"
+  "boostpythonExamples.pdb"
+  "boostpythonExamples"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/boostpythonExamples.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
